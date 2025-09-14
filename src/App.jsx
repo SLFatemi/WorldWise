6b559ace5b21a3265e3e402ca71ba793f2622310
SLFatemi <sinalotfifatemi@gmail.com>
2025-09-14 17:50:40 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Product from "./pages/Product.jsx";
+import "./App.css";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path={"/"} element={<Home />} />
+				<Route path={"product"} element={<Product />} />
+				<Route path={"pricing"} element={<Pricing />} />
+				<Route path={"*"} element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+    return <div>WorldWise</div>
+}
+
+export default Home
+
